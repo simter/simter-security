32c0dc8884711905d7a124ae2c38ffda54eb0eea
RJ.Hwang <rongjihuang@gmail.com>
2017-04-25 09:58:29 +0800

    add base security service

---
+++ src/main/java/tech/simter/security/SecurityService.java
@@ -0,0 +1,61 @@
+package tech.simter.security;
+
+/**
+ * System Security
+ *
+ * @author RJ 2017-04-25
+ */
+public interface SecurityService {
+  /**
+   * The key that store roles in context
+   */
+  String CONTEXT_KEY_ROLES = "ROLES";
+
+  /**
+   * Determine whether the context has a specified role
+   *
+   * @param role the role's identify
+   * @return true if has the role, otherwise return false
+   */
+  boolean hasRole(String role);
+
+  /**
+   * Determine whether the context has any specified roles
+   *
+   * @param roles the role's identifies
+   * @return true if has any one role, otherwise return false
+   */
+  boolean hasAnyRole(String... roles);
+
+  /**
+   * Determine whether the context has all the specified roles
+   *
+   * @param roles the role's identifies
+   * @return true if has all roles, otherwise return false
+   */
+  boolean hasAllRole(String... roles);
+
+  /**
+   * Verify whether the context has a specified role
+   *
+   * @param role the role's identify
+   * @throws SecurityException if the context has not the specified role
+   */
+  void verifyHasRole(String role) throws SecurityException;
+
+  /**
+   * Verify whether the context has any specified roles
+   *
+   * @param roles the role's identifies
+   * @throws SecurityException if the context has not any specified role
+   */
+  void verifyHasAnyRole(String... roles) throws SecurityException;
+
+  /**
+   * Verify whether the context has all the specified roles
+   *
+   * @param roles the role's identifies
+   * @throws SecurityException if the context has not all the specified roles
+   */
+  void verifyHasAllRole(String... roles) throws SecurityException;
+}
---
+++ src/main/java/tech/simter/security/SecurityServiceImpl.java
@@ -0,0 +1,59 @@
+package tech.simter.security;
+
+import tech.simter.Context;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * System Security Implementation by Context
+ *
+ * @author RJ 2017-04-25
+ */
+@Named
+@Singleton
+public class SecurityServiceImpl implements SecurityService {
+  public boolean hasRole(String role) {
+    Set<String> contextRoles = Context.get(CONTEXT_KEY_ROLES);
+    return contextRoles != null && contextRoles.contains(role);
+  }
+
+  public boolean hasAnyRole(String... roles) {
+    Set<String> contextRoles = Context.get(CONTEXT_KEY_ROLES);
+    if (contextRoles == null || roles == null) return false;
+    for (String role : roles) {
+      if (contextRoles.contains(role)) return true;
+    }
+    return false;
+  }
+
+  public boolean hasAllRole(String... roles) {
+    Set<String> contextRoles = Context.get(CONTEXT_KEY_ROLES);
+    if (contextRoles == null || roles == null) return false;
+    return contextRoles.containsAll(Stream.of(roles).collect(Collectors.toSet()));
+  }
+
+  public void verifyHasRole(String role) throws SecurityException {
+    Set<String> contextRoles = Context.get(CONTEXT_KEY_ROLES);
+    if (contextRoles == null || !contextRoles.contains(role)) throw new SecurityException();
+  }
+
+  public void verifyHasAnyRole(String... roles) throws SecurityException {
+    Set<String> contextRoles = Context.get(CONTEXT_KEY_ROLES);
+    if (contextRoles == null || roles == null) throw new SecurityException();
+    for (String role : roles) {
+      if (contextRoles.contains(role)) return;
+    }
+    throw new SecurityException();
+  }
+
+  public void verifyHasAllRole(String... roles) throws SecurityException {
+    Set<String> contextRoles = Context.get(CONTEXT_KEY_ROLES);
+    if (contextRoles == null || roles == null) throw new SecurityException();
+    if (!contextRoles.containsAll(Stream.of(roles).collect(Collectors.toSet())))
+      throw new SecurityException();
+  }
+}
